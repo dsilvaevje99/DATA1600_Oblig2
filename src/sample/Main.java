f5890bb708d5e93a3bf42d3439e659a5df0d098e
Daniel Silva <daniel@visualyst.co>
2020-03-04 11:42:44 +0100

    Task 4 start.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,11 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("DATA1600");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
+        launch(args);
+
+        //OPPGAVE 1 - Rekursjon
         Oppg1_Rekursjon oppg1 = new Oppg1_Rekursjon();
         System.out.println(oppg1.sum(5,0));
         System.out.println(oppg1.pow(5,5));
